e3bc6e9216a60456595055713c39b5e355ce0fc5
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-04-17 11:20:47 +0600

    setup all necessary elements and install all pakages

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQc1Nfh_ET6AXGmGi2YWnRxGWB5fuRZBc",
+  authDomain: "dental-care-f051f.firebaseapp.com",
+  projectId: "dental-care-f051f",
+  storageBucket: "dental-care-f051f.appspot.com",
+  messagingSenderId: "1077775126295",
+  appId: "1:1077775126295:web:7a10798f919de0bd64b152"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
