d286d46276a042ae0ae1bcc8aba0c209058b0a6e
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-04-18 15:48:05 +0600

    services add home pase and services page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import useServices from '../hooks/useServices';
+import ServiceCard from '../ServiceCard/ServiceCard';
 import Banner from '../SignIn/Banner/Banner';
+import './Home.css'
 
 const Home = () => {
+    const [services] = useServices();
     return (
         <div>
             <Banner></Banner>
+            <section className='review demo mt-5'>
+                    <h1 className='home-card-title mb-3 text-center'>Some Demo Review</h1>
+                    <div className='home-card-container container'>
+                    {
+                       services.slice(0,3).map(services =><ServiceCard
+                        key={services.id}
+                        services = {services}
+                        ></ServiceCard> )
+            
+                    }
+                    </div>
+                   {/* <Link  to="/review">See All Review</Link> */}
+                </section>
         </div>
     );
 };
--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './ServiceCard.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ServiceCard.css";
 
 const ServiceCard = (props) => {
-    const { name, img, description, rating } = props.services;
-    return (
-        <div>
-        <div className="card h-100 shadow-lg">
-            <img src={img} className="card-img-top cart-img" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title fs-4 fw-bold">Name : {name}</h5>
-                <h5 className="card-title">Rating : {rating}</h5>
-                <p className="card-text text-start fs-5"><span className="fs-5 fw-bold ">Description:{description} </span></p>
-            </div>
+  const { name, img, description, rating, Fee } = props.services;
+  return (
+    <div>
+      <div className="card h-100 shadow-lg">
+        <img src={img} className="card-img-top cart-img" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title fs-4 fw-bold">Name : {name}</h5>
+          <h5 className="card-title">Rating : {rating}</h5>
+          <h5 className="card-title">Treatment Fee : {Fee}</h5>
+          <p className="card-text text-start">
+            <span className="fs-5 fw-bold">Description: </span>
+            {description.length > 150
+              ? description.slice(0, 130)
+              : description}..........{" "}
+          </p>
+          <div>
+            <Link to="/appointment">
+              <button className="btn-lg btn btn-primary fs-5 fw-bold w-100">
+                Appointment Book
+              </button>
+            </Link>
+          </div>
         </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default ServiceCard;
